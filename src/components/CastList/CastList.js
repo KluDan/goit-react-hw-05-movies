cb6ebf2f7f3d96754f9d223e22d83ce5d328fbf5
Danylo Kliushnychenko <danylktv2@gmail.com>
2023-09-05 18:29:01 +0100

    Update CastList.js

--- src/components/CastList/CastList.js
+++ src/components/CastList/CastList.js
@@ -5,6 +5,9 @@ export const CastList = ({ cast, loading }) => {
   if (loading) {
     return <LoadingSpinner />;
   }
+  if (!Array.isArray(cast) || cast.length === 0) {
+    return <div>Reviews data is not available.</div>;
+  }
   const placeholderImage =
     'https://static.displate.com/857x1200/displate/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg';
   return (
